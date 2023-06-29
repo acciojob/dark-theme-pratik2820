052014efb0f8e0294885f4f14cba6ba2eeccdcaf
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-06-29 11:17:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here
+function swapTheme() {
+  var appDiv = document.getElementById("app");
+  var swapButton = document.getElementById("swap");
+
+  if (appDiv.classList.contains("day")) {
+    appDiv.classList.remove("day");
+    appDiv.classList.add("night");
+
+    swapButton.classList.remove("button_day");
+    swapButton.classList.add("button_night");
+  } else {
+    appDiv.classList.remove("night");
+    appDiv.classList.add("day");
+
+    swapButton.classList.remove("button_night");
+    swapButton.classList.add("button_day");
+  }
+}
+
+
+
+
+
+
 
